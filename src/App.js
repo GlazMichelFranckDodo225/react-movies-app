7b123d41a4c7a0127f5dc86b401665a243bf5fa5
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-06 19:00:40 +0200

    Using Fragments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
 const App = () => {
-  const name = "John"
-  const isNameShowing = false
+  const name = "Jane"
 
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing ? name : "someone"} !</h1>
+      <h1>Hello, {2 + 2} !</h1>
+      {name ? (
+        <>
+          {name}
+        </>
+      ) : (
+        <>
+          <h1>Name is not defined</h1>
+          <h1>There is no name</h1>
+        </>
+      )}
     </div>
   );
 }
