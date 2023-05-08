1f6656bc882cef28d4ceed415fbe03067968d162
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-08 09:35:43 +0200

    The 'useState' Hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name : {props.name}</h1>
-      <h2>Last Name : {props.lastname}</h2>
-      <h2>Age : {props.age}</h2>
-    </>
-  )
-}
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <Person 
-        name="John" 
-        lastname="Doe" 
-        age={25} 
-      />
-      <Person 
-        name="Mary" 
-        lastname="Doe" 
-        age={30} 
-      />
+      <button onClick={() => setCounter(prevCount => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(prevCount => prevCount + 1)}>+</button>
     </div>
   );
 }
