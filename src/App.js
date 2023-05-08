8e5f9ea08bc2b3a0f44d7dd554391cc1766dca44
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-08 17:07:01 +0200

    Modification of the App.css file: h3 => h2. Recovery of data from the movie "Amazing Spiderman Syndrome". Setting up the <div className="container">

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import SearchIcon from './search.svg'
 // OMDb Api Key ==> 6ac94508
 const API_URL = 'http://www.omdbapi.com?apikey=6ac94508'
 
+const movie1 = {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
 const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
@@ -33,6 +41,26 @@ const App = () => {
                     onClick={() => {}}
                 />
             </div>  
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img 
+                            src={movie1.Poster != "N/A" ? movie1.Poster : "https://via.placeholder.com/400"} 
+                            alt={movie1.Title} 
+                        />
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h2>{movie1.Title}</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
