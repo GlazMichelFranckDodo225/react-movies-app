4fffd34457aa407d7eca46b1ebeb661e0fbfabc6
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-08 15:40:25 +0200

    Implementation of OMDb API data recovery. Upgrading index.js file code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import {useEffect} from "react";
 
 // OMDb Api Key ==> 6ac94508
 const API_URL = 'http://www.omdbapi.com?apikey=6ac94508'
 
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, [])
+
     return(
         <h1>App</h1>
     )
