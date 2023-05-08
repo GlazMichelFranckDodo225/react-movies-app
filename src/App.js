e3ad531e00c9320809982067d46b367f9e7c7c82
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-08 17:18:56 +0200

    Extracting the <div className="movie"> from the App.js component to add it to the new MovieCard.jsx component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useEffect} from "react";
 import './App.css'
 import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
 
 // OMDb Api Key ==> 6ac94508
 const API_URL = 'http://www.omdbapi.com?apikey=6ac94508'
@@ -43,23 +44,7 @@ const App = () => {
             </div>  
 
             <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-
-                    <div>
-                        <img 
-                            src={movie1.Poster != "N/A" ? movie1.Poster : "https://via.placeholder.com/400"} 
-                            alt={movie1.Title} 
-                        />
-                    </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h2>{movie1.Title}</h2>
-                    </div>
-                </div>
+               <MovieCard />     
             </div>
         </div>
     )
